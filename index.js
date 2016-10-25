3fbf680cb08d8b55dbe5ee4f38180b2f73513385
Pankaj Patel <patel.pankaj@live.in>
2016-10-25 23:27:06 +0530

    demo server up

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-var hapi = require('hapi');
+const Hapi = require('hapi');
+
+// Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({ 
+    host: 'localhost', 
+    port: 8000 
+});
+
+// Add the route
+server.route({
+    method: 'GET',
+    path:'/hello', 
+    handler: function (request, reply) {
+
+        return reply('hello world');
+    }
+});
+server.route({
+    method: 'GET',
+    path:'/api/{username}', 
+    handler: function (request, reply) {
+
+        return reply({q:request.query, p: request.params});
+    }
+});
+
+server.route({
+    method: ['PUT', 'GET'],
+    path: '/api/{username}/friends',
+    handler: function(request, reply) {
+        const username = encodeURIComponent(request.params.username);
+        return reply(username)
+    }
+});
+// Start the server
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
